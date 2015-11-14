20e8ddb699e15e52a5813816fc25f02185953f32
Small_D <weerayutlek@hotmail.com>
2015-11-14 15:00:37 +0700

    Sound Effect Successful

--- app/src/main/java/co/small/small_d/butraffic/MainActivity.java
+++ app/src/main/java/co/small/small_d/butraffic/MainActivity.java
@@ -1,13 +1,61 @@
 package co.small.small_d.butraffic;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    //ประกาศตัวแปร
+    private ListView trafficListView;
+    private Button aboutMeButton;
+
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //Bind Widget
+        bindWidget();
+
+        //Button Controller
+
+        buttonController();
+
+
+    } //เมทอดหลัก
+
+    private void buttonController() {
+        aboutMeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                //Sound Effect
+                MediaPlayer buttonPlayer = MediaPlayer.create(getBaseContext(), R.raw.sheep);
+                buttonPlayer.start();
+
+
+                //Intent to WebView
+
+
+            } //Event
+        });
     }
-}
+
+    private void bindWidget() {
+        trafficListView = (ListView) findViewById(R.id.listView);
+        aboutMeButton = (Button) findViewById(R.id.button);
+    }
+
+} //คลาสหลัก
